59c6889eae51f457ab4a2922fe14b165cfac87c8
bvkatwijk <BorisK@topdesk.com>
2017-07-14 16:26:01 +0200

    Add MicroService defaults

---
+++ src/main/java/org/bvkatwijk/lib/micro/MicroServiceDefaults.java
@@ -0,0 +1,17 @@
+package org.bvkatwijk.lib.micro;
+
+public final class MicroServiceDefaults {
+
+	public static final String SERVLETS_URL_PATH = "/api/*";
+	public static final String HOME_PAGE_FOLDER = "web";
+	public static final String HOME_PAGE_FILENAME = "index.html";
+	public static final String APPLICATION_NAME = "Application";
+	public static final String SERVLET_PACKAGE = "org.bvkatwijk.api";
+	public static final int PORT_NUMBER = 8080;
+	public static final Configuration CONFIGURATION = new NoConfiguration();
+
+	private MicroServiceDefaults() {
+		throw new AssertionError(this.getClass().getSimpleName() + " not to be instantiated.");
+	}
+
+}
